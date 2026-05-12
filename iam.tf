d7ac966c44510c8da08c536e3606b99f45eaf69c
Telishahid <shahidteli35@gmail.com>
2026-05-13 00:57:33 +0530

    Change in Iam file

--- iam.tf
+++ iam.tf
@@ -1,30 +1,25 @@
-
 resource "aws_iam_role" "our-iam-role" {
-    name = "Terraform-Admin"
-    assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": [
-          "ec2.amazonaws.com"
-        ]
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+  name = "Terraform-Admin"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
   
 
 
-resource "aws_iam_role_policy_attachment" "ec2-policy" {
-    role = aws_iam_role.our-iam-role.name
-    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+resource "aws_iam_role_policy_attachment" "admin_attach" {
+  role       = aws_iam_role.our-iam-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 /*
